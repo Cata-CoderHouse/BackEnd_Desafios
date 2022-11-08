cd7d3b6846c6b6c558036b3dad49932ce09de3f3
Catalina Hernandez <cataxto@gmail.com>
2022-11-08 18:49:33 -0500

    CRUD en firebase

--- Desafio10-SegundaEntrega/firebase.js
+++ Desafio10-SegundaEntrega/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {collection, doc, addDoc, getFirestore} from 'firebase/firestore'; //Agrego esta linea, getFire.. para consultar la bdd
+import {collection, doc, addDoc, getFirestore, getDoc, updateDoc} from 'firebase/firestore'; //Agrego esta linea, getFire.. para consultar la bdd
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,12 +21,9 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
-
-//Agrego esta funcion
-
 const db = getFirestore();
 
+//create todos los productos
 const cargarBDD= async()=>{
     const promise = await fetch('./json/productos.json') //consulto archivo local
     const productos = await promise.json();
@@ -44,4 +41,40 @@ const cargarBDD= async()=>{
     })
 }
 
+//create un producto 
+const createProducto = async(objProd)=>{
+    const estado = await addDoc(collection(db,"productos"),{
+        fecha: objProd.timestamp,
+        nombre: objProd.nombre, 
+        descripcion: objProd.descripcion,
+        codigo: objProd.codigo,
+        imagen:objProd.imagen,
+        precio: objProd.precio,
+        inventario: objProd.inventario
+    })
+}
+
+const getProducto = async (id) =>  {
+    const producto = await getDoc(doc(db,"productos",id));
+    const prod = [producto.id, producto.data()];
+    return prod;
+}
+
+const getProductos = async()=>{
+    const productos = await getDocs(collection(db,"productos"));
+    const prod = productos.docs.map((prod)=>{
+        return [prod.id, prod.data()];
+    })
+    return prod;
+}
+
+const updatProducto = async(id, info) => {
+    const estado = await updateDoc(doc(db,"productos",id), info);
+    return estado;
+}
+
+const deleteProducto = async(id) =>{
+    const estado = await deleteDoc(doc(db,"productos",id));
+    return estado;
+}
 export {cargarBDD}
